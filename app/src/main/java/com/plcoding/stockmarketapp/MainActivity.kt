06b571d1d5c23bd7a7feea3b97494b21027d755f
luisFebro <mr.febro@gmail.com>
2024-01-04 08:43:10 -0300

    updating all file structure + complete data

--- app/src/main/java/com/plcoding/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.plcoding.stockmarketapp.ui.theme.StockMarketAppTheme
 
+// ref: https://www.youtube.com/watch?v=uLs2FxFSWU4
+// alpha vantage API: IT6GV5CU6TZBJA6B
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
